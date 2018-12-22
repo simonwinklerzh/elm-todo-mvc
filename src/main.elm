37a165cf27529097a0e1aa8de3682c4fa5e93895
Simon Winkler <simon.winkler@viu.ch>
2018-12-22 16:07:11 +0100

    Initial type definitions

--- src/main.elm
+++ src/main.elm
@@ -5,6 +5,30 @@ import Html
 import Html.Events
 import Html.Attributes
 
+type alias Entry =
+    { text : Int
+    , checked : Bool
+    , id : Int
+    }
+
+type Filter
+    = All
+    | Active
+    | Completed
+
+type alias Model =
+    { entries : List Entry
+    , input : Maybe String
+    , filter : Filter
+    }
+
+type Msg
+    = Input
+    | Add
+    | Mark
+    | Remove
+    | Filter
+
 main : Html.Html msg
 main = Html.h1
     []
