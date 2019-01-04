60ed5fb196c3edac7364db9a013be55119ff3b98
Simon Winkler <simon.winkler@viu.ch>
2019-01-04 17:31:48 +0100

    Implement basic remove function. Use an id generator function instead of entries.length which was not correct

--- src/main.elm
+++ src/main.elm
@@ -2,15 +2,17 @@ module Main exposing (main)
 
 import Browser
 import Html
-import Html.Events exposing ( onInput, onSubmit )
+import Html.Events exposing ( onInput, onSubmit, onClick )
 import Html.Attributes exposing ( class, attribute, type_, value, placeholder )
 
 -- Model
 
+type alias Id = Int
+
 type alias Entry =
     { text : String
     , checked : Bool
-    , id : Int
+    , id : Id
     }
 
 type Filter
@@ -22,13 +24,14 @@ type alias Model =
     { entries : List Entry
     , input : String
     , filter : Filter
+    , current_id : Id
     }
 
 type Msg
     = Input String
     | Add
     | Mark
-    | Remove
+    | Remove Entry
     | Filter
 
 initModel : Model
@@ -36,6 +39,7 @@ initModel =
     { entries = []
     , input = ""
     , filter = All
+    , current_id = 0
     }
 
 -- Update
@@ -49,18 +53,37 @@ update msg model =
             add model
         Mark ->
             model
-        Remove ->
-            model
+        Remove entry ->
+            remove model entry
         Filter ->
             model
 
 add : Model -> Model
 add model =
     let 
+        new_id = 
+            model.current_id + 1
         new_entry =
-            { text = model.input, checked = False, id = List.length model.entries + 1 }
+            { text = model.input, checked = False, id = new_id }
     in
-        { model | entries = new_entry :: model.entries }
+        { model 
+            | entries = new_entry :: model.entries 
+            , current_id = new_id
+        }
+
+is_same_entry : Entry -> Entry -> Bool
+is_same_entry a b = 
+    a.id == b.id
+
+is_not_same_entry : Entry -> Entry -> Bool
+is_not_same_entry a b = not (is_same_entry a b)
+
+remove : Model -> Entry -> Model
+remove model entry =
+    let 
+        new_entries = List.filter (is_not_same_entry entry) model.entries
+    in
+        { model | entries = new_entries }
 
 -- View
 
@@ -95,7 +118,13 @@ todos_list_entry entry =
         [ class "todos__list-entry" 
         , attribute "data-id" (String.fromInt entry.id)
         ]
-        [ Html.text entry.text ]
+        [ Html.text ( entry.text ++ "_" ++ (String.fromInt entry.id) )
+        , Html.button
+            [ type_ "button"
+            , onClick (Remove entry)
+            ]
+            [ Html.text "X" ]
+        ]
 
 todos_list : List Entry -> Html.Html Msg
 todos_list entries =
